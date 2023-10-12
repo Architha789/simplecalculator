da02461ad172b2bc7b3462db10a9ed1a2e040fae
Architha789 <archithak78@gmail.com>
2023-10-12 23:43:29 +0530

    index page and js page added

---
+++ if.js
@@ -0,0 +1,26 @@
+function divisible(){
+    a=parseInt(document.getElementById('num1').value);
+    if(a%3 == 0 ){
+        result="Divisible by 3";
+    }
+    else if(a%7 == 0){
+        result="Divisible by 7";
+    }
+    else{
+        document.getElementById("p1").innerHTML="Not divisile by 3 and 7";
+    }
+    document.getElementById("p1").innerHTML=result;
+}
+function compare(){
+    alpha=parseInt(document.getElementById('num2').value);
+
+    if( alpha == "a" || alpha == "e" || alpha =="i" || alpha == "o" || alpha == "u"){
+        result="Vowel";
+
+    } 
+    else{
+        result="Consonant";
+    }
+    document.getElementById("p2").innerHTML=result;
+}
+
